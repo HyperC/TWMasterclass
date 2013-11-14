97fa09555ce8220bf2a52fadee04b6f79980bafa
Thomas Visser <thomas.visser@gmail.com>
2013-11-14 15:32:35 +0100

    Project set-up

---
+++ TWMasterclass/Classes/Application/TWMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWMAppDelegate.h
+//  TWMasterclass
+//
+//  Created by Thomas Visser on 14/11/13.
+//  Copyright (c) 2013 Touchwonders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TWMasterclass/Other Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TWMasterclass
+//
+//  Created by Thomas Visser on 14/11/13.
+//  Copyright (c) 2013 Touchwonders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWMAppDelegate class]));
+    }
+}
